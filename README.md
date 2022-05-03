b8c7efe6f688f7967778ba825941ae4367f82e75
Teloschet <chrisaleiro@gmail.com>
2022-05-03 17:58:33 -0300

    .

---
+++ README.md
@@ -0,0 +1,5 @@
+# telos-widget flutter
+
+- it's a project for study using flutter/dart.
+
+Latest update 17:55 03/05/2022.
---
+++ android/app/src/main/kotlin/com/example/telos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.telos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
